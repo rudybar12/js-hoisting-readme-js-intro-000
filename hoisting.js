6ddbb92506d56d6acaf891d814bf61d69df521d4
rodolfo Barrientos <rudybar12@yahoo.com>
2020-06-17 22:30:57 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -19,8 +19,8 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
   function sayMy() {
+    var name = " Kristin";
     console.log("name");
-    var name = "Kristin";
   }
   sayMyName();
 }
